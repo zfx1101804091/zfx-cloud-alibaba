79e4ee5c4b08dd967891f2b816ea5498f7cbc52a
guapizheng <1875586562@qq.com>
2020-08-18 14:19:58 +0800

    基于Nacos的服务提供者

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/amplesky/cloud/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.amplesky.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zheng-fx
+ * @time: 2020/8/18 0018 14:06
+ */
+@SpringBootApplication
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/amplesky/cloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.amplesky.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: zheng-fx
+ * @time: 2020/8/18 0018 14:08
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
